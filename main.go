9b93214df108d6d6eef34895b011967aad838230
Joe Chen <jc@unknwon.io>
2022-03-30 11:27:51 +0800

    Update main.go

--- main.go
+++ main.go
@@ -12,3 +12,9 @@ func main() {
 func outdatedReplace(s string) string {
 	return strings.Replace(s, "\\", "/", -1)
 }
+
+func unusedRangeValue(s []string) {
+	for i, _ := range s {
+		fmt.Println(s[i])
+	}
+}
